b30d5f041c55f457105d7eb09bc272b1e203a4eb
Retraxi <raphael_tanai@dlsu.edu.ph>
2025-03-30 16:01:28 +0800

    Second commit test

---
+++ Networked-Producer-And-Consumer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Networked_Producer_And_Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string variable = "temporary";        
+    }
+}
